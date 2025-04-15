331e8f5da7132820817a6120ad9f556679c333a5
Sayyed-rehan <rehansayyed1216@gmail.com>
2025-04-15 11:21:06 +0530

    styling updated in modal

--- Task_2/server/server.js
+++ Task_2/server/server.js
@@ -11,7 +11,7 @@ const ___dirname = path.resolve()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 
 // app.get("/", async (req, res) => {
 //     const data = await todo.find()
